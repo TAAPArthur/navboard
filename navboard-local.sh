61a3bc65fb1479f7d746d6513c28d47f4d7c4dbf
Arthur Williams <taaparthur@gmail.com>
2021-04-24 18:03:15 -0700

    Run every shell script in NAVBOARD_CONFIG_HOME
    This was added to aid in users generated their own boards dynamically.
    navboard-local -r will first run the shells and pass in the destination
    directory and then build the executable.

--- navboard-local.sh
+++ navboard-local.sh
@@ -4,6 +4,9 @@ NAVBOARD_CONFIG_HOME=${XDG_CONFIG_DIR:-$HOME/.config}/navboard
 NAVBOARD_LOCAL="$NAVBOARD_CONFIG_HOME/navboard-local"
 if [ "$1" = "-r" ] || [ "$1" = "--recompile" ]; then
     mkdir -p "$NAVBOARD_CONFIG_HOME"
+    for f in *.sh; do
+        [ -x "$f" ] && $f "$NAVBOARD_CONFIG_HOME"
+    done
     ${CC:-cc} -o "$NAVBOARD_LOCAL" -lnavboard "$NAVBOARD_CONFIG_HOME"/*.c -lX11 -lxcb -lxcb-ewmh -lxcb-icccm -lxcb-xtest -lX11-xcb -lXft -lm
 else
     exec "$NAVBOARD_LOCAL" "$@"
