e8d4c14bc80499b9adc83077bd81fa6230923c8e
Arthur Williams <taaparthur@gmail.com>
2021-04-24 22:06:51 -0700

    fixup! fixup! fixup! Run every shell script in NAVBOARD_CONFIG_HOME

--- navboard-local.sh
+++ navboard-local.sh
@@ -4,10 +4,11 @@ NAVBOARD_CONFIG_HOME=${XDG_CONFIG_DIR:-$HOME/.config}/navboard
 NAVBOARD_LOCAL="$NAVBOARD_CONFIG_HOME/navboard-local"
 if [ "$1" = "-r" ] || [ "$1" = "--recompile" ]; then
     mkdir -p "$NAVBOARD_CONFIG_HOME"
-    for f in "$NAVBOARD_CONFIG_HOME"/*.sh; do
+    cd "$NAVBOARD_CONFIG_HOME" || exit 1
+    for f in *.sh; do
         [ -x "$f" ] && "./$f" "$NAVBOARD_CONFIG_HOME"
     done
-    ${CC:-cc} -o "$NAVBOARD_LOCAL" -lnavboard "$NAVBOARD_CONFIG_HOME"/*.c -lX11 -lxcb -lxcb-ewmh -lxcb-icccm -lxcb-xtest -lX11-xcb -lXft -lm
+    ${CC:-cc} -o "$NAVBOARD_LOCAL" -lnavboard -lX11 -lxcb -lxcb-ewmh -lxcb-icccm -lxcb-xtest -lX11-xcb -lXft -lm -- *.c
 else
     exec "$NAVBOARD_LOCAL" "$@"
 fi
